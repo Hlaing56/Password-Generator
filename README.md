06180e7cad1b290759e75e915ad38a95e6e2fe0b
Hlaing Aung <betahba@gmail.com>
2021-10-08 14:25:48 -0400

    can make a password with only one random function

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Password-Generator
+
+basic form -
+pick how long the password is and generate random upper case letters to that length
+now print into textbox
+
+what I have right now-
+when I click the generate buttom it gives me a random uppercase letter
+
+who do I make is so its based off the length I ask for
+
+we need a return
+
+we did it. Now I can generate a password with just upper case letters.
+
+Now do that with more than just upper case letters
+
+next basic step-
+just have it so it take all the four different randoms, no need to be picky yet
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
+//only when you click the word generate the prompt pops up
+var generateBtn = document.querySelector("#generate");
 
-//give a random lowercase letter
-function randomLower() {
-    var result = " ";
-    var characters = "abcdefghijklmnopqrstuvwxyz";
-    result = characters.charAt(Math.floor(Math.random()*26) );
-    return result;
-}
+// var randomFun = {
+//     lower: randomLower,
+//     upper: randomUpper,
+//     num: randomNum,
+//     sym: randomSym
+// };
 
-//give a random uppercase letter
+
+// //give a random lowercase letter
+// function randomLower() {
+//     var result = " ";
+//     var characters = "abcdefghijklmnopqrstuvwxyz";
+//     result = characters.charAt(Math.floor(Math.random()*26) );
+//     return result;
+// }
+
+// //give a random uppercase letter
 function randomUpper() {
     var result = " ";
     var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,24 +25,44 @@ function randomUpper() {
     return result;
 }
 
-//give a random number
-function randomNum() {
-    var result = " ";
-    var characters = "0123456789";
-    result = characters.charAt(Math.floor(Math.random()*10) );
-    return result;
+// //give a random number
+// function randomNum() {
+//     var result = " ";
+//     var characters = "0123456789";
+//     result = characters.charAt(Math.floor(Math.random()*10) );
+//     return result;
+// }
+
+// //give a random symbol
+// function randomSym() {
+//     var result = " ";
+//     var characters = "!@#$%^&*()";
+//     result = characters.charAt(Math.floor(Math.random()*10) );
+//     return result;
+// }
+
+function makePassword() {
+
+    var generatedPassword = "";
+
+    var howLong = window.prompt("choose a length of at least 8 characters and no more than 128 characters for your password");
+    //this would be how long my password is 
+    howLong = parseInt(howLong);
+    
+    for(var i = 0; i < howLong; i++){
+        generatedPassword += randomUpper();
+    }
+
+    return generatedPassword;
 }
 
-//give a random symbol
-function randomSym() {
-    var result = " ";
-    var characters = "!@#$%^&*()";
-    result = characters.charAt(Math.floor(Math.random()*10) );
-    return result;
+
+function writePassword(){
+    var generatedPass = makePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = generatedPass;
 }
 
 
-
-
-
-console.log(randomSym());
+//listens for the word generate to be clicked
+generateBtn.addEventListener("click", writePassword);
