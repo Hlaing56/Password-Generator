1291c9e377a9401929a1a063c7c885b6273e9cdd
Hlaing Aung <betahba@gmail.com>
2021-10-08 21:32:59 -0400

    added everything needed

--- README.md
+++ README.md
@@ -17,3 +17,9 @@ Now do that with more than just upper case letters
 
 next basic step-
 just have it so it take all the four different randoms, no need to be picky yet
+
+so simple that it took me 1 hour and a nap
+
+be simple dont have to do things you haven't learned JUST GET IT TO WORK
+
+everything works except when I say I want 9 characters for my password with all 4 random functions it prints out 9 characters but only 3 of the random functions
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,20 @@
 //only when you click the word generate the prompt pops up
 var generateBtn = document.querySelector("#generate");
 
-// var randomFun = {
-//     lower: randomLower,
-//     upper: randomUpper,
-//     num: randomNum,
-//     sym: randomSym
-// };
-
+var randomFun = {
+    lower: randomLower,
+    upper: randomUpper,
+    num: randomNum,
+    sym: randomSym
+}
 
 // //give a random lowercase letter
-// function randomLower() {
-//     var result = " ";
-//     var characters = "abcdefghijklmnopqrstuvwxyz";
-//     result = characters.charAt(Math.floor(Math.random()*26) );
-//     return result;
-// }
+function randomLower() {
+    var result = " ";
+    var characters = "abcdefghijklmnopqrstuvwxyz";
+    result = characters.charAt(Math.floor(Math.random()*26) );
+    return result;
+}
 
 // //give a random uppercase letter
 function randomUpper() {
@@ -26,34 +25,72 @@ function randomUpper() {
 }
 
 // //give a random number
-// function randomNum() {
-//     var result = " ";
-//     var characters = "0123456789";
-//     result = characters.charAt(Math.floor(Math.random()*10) );
-//     return result;
-// }
+function randomNum() {
+    var result = " ";
+    var characters = "0123456789";
+    result = characters.charAt(Math.floor(Math.random()*10) );
+    return result;
+}
 
 // //give a random symbol
-// function randomSym() {
-//     var result = " ";
-//     var characters = "!@#$%^&*()";
-//     result = characters.charAt(Math.floor(Math.random()*10) );
-//     return result;
-// }
+function randomSym() {
+    var result = " ";
+    var characters = "!@#$%^&*()";
+    result = characters.charAt(Math.floor(Math.random()*10) );
+    return result;
+}
 
 function makePassword() {
-
     var generatedPassword = "";
 
     var howLong = window.prompt("choose a length of at least 8 characters and no more than 128 characters for your password");
     //this would be how long my password is 
-    howLong = parseInt(howLong);
-    
-    for(var i = 0; i < howLong; i++){
-        generatedPassword += randomUpper();
-    }
 
-    return generatedPassword;
+    howLong = parseInt(howLong);
+
+
+    if(howLong > 7 && howLong < 129){
+
+        var lowConfirm = window.confirm("Would you like lowercase letters in your password?");
+        var upperConfirm = window.confirm("Would you like uppercase letters in your password?");
+        var numConfirm = window.confirm("Would you like numbers in your password?");
+        var symConfirm = window.confirm("Would you like symbols in your password?");
+    
+    
+        var typesCount = lowConfirm + upperConfirm + numConfirm + symConfirm;
+    
+        console.log("types:", typesCount);
+    
+        if(lowConfirm){
+            for(var i = 0; i < howLong/typesCount; i ++){
+                generatedPassword += randomLower();
+                }
+        }
+    
+        if(upperConfirm){
+            for(var i = 0; i < howLong/typesCount; i ++){
+                generatedPassword += randomUpper();
+                }
+        }
+    
+        if(numConfirm){
+            for(var i = 0; i < howLong/typesCount; i ++){
+                generatedPassword += randomNum();
+                }
+        }
+        
+        if(symConfirm){
+            for(var i = 0; i < howLong/typesCount; i ++){
+                generatedPassword += randomSym();
+                }
+        }
+      
+        return generatedPassword.slice(0, howLong);
+
+    } else {
+        window.alert("I SAID PICK BETWEEN 8 TO 128 CHARACTERS");
+    }
+        
 }
 
 
